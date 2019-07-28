a4c16c9a81e3df6f07ed7df012d9733372cfcbed
asdyxc123 <703660472@qq.com>
2019-07-28 22:46:52 +0800

    前端提交

---
+++ web-client/src/main/webapp/module/js/apiconfig.js
@@ -0,0 +1,11 @@
+layui.define(function (exports) {
+  const baseUrl = '/api/';
+  const apiconfig = {
+    user: {
+      login: baseUrl + 'user/login',
+      getUsers: baseUrl + 'user/getusers',
+      getMenus: baseUrl + 'getmenus',
+    }
+  };
+  exports('apiconfig', apiconfig);
+});
